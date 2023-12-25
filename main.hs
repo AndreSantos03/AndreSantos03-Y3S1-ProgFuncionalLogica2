ba62ddb74c96323f45c0b1734acc0eeea9bff7cb
up202004513@up.pt <up202004513@up.pt>
2023-12-25 01:09:23 +0000

    all tests working (part 1)

--- main.hs
+++ main.hs
@@ -9,7 +9,7 @@ data Inst =
   deriving Show
 type Code = [Inst]
 
-data StackVal = IVal Integer | BVal Bool | TVal String deriving Show
+data StackVal = IVal Integer | BVal Bool | TVal String deriving (Show, Eq)
 type Stack = [StackVal]
 
 tt :: StackVal
@@ -27,14 +27,13 @@ createEmptyState :: State
 createEmptyState = (createEmptyStack, [])
 
 stack2Str :: Stack -> String
-stack2Str = intercalate "," . reverse . map showStackVal
+stack2Str stack = intercalate "," (map showStackVal stack)
+
 
 showStackVal :: StackVal -> String
 showStackVal (IVal n) = show n
-showStackVal (BVal b) = show b
-showStackVal (TVal s) = s
-
-
+showStackVal (BVal True) = "True"
+showStackVal (BVal False) = "False"
 
 state2Str :: State -> String
 state2Str (_, store) =
@@ -45,6 +44,18 @@ state2Str (_, store) =
     showVarVal (var, BVal True) = var ++ "=True"
     showVarVal (var, BVal False) = var ++ "=False"
     showVarVal (var, TVal s) = var ++ "=" ++ s  -- This line can actually be removed if you're not using TVal anymore.
+
+evaluateCondition :: Code -> State -> Bool
+evaluateCondition condition state = 
+  let (_, stack, _) = run (condition, [], state) -- You might need to adjust this call according to how 'run' is defined
+  in case stack of
+       (BVal b : _) -> not b -- Assuming the condition leaves a boolean on top of the stack indicating whether to exit the loop
+       _ -> error "Condition code did not leave a boolean value on the stack"
+
+-- Assuming the top of the stack is a BVal containing the condition result
+getConditionResult :: Stack -> Bool
+getConditionResult (BVal result : _) = result
+getConditionResult _ = error "Condition did not evaluate to a boolean value"
 
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run ([], stack, state) = ([], stack, state)
@@ -67,8 +78,44 @@ run (Fals:code, stack, state) =
     trace ("Fals") $
     run (code, BVal False : stack, state)
 run ((Store var):code, val:stack, (s, store)) =
-    trace ("Store " ++ var) $
-    run (code, stack, (s, (var, val) : store))
+    trace ("Store " ++ var ++ " " ++ showStackVal val ++ " Pre-store: " ++ state2Str (s, store)) $
+    let updatedStore = updateStore var val store
+    in trace ("Post-store: " ++ state2Str (s, updatedStore)) $
+       run (code, stack, (s, updatedStore))
+  where
+    updateStore var val [] = [(var, val)]
+    updateStore var val ((v,sVal):vs)
+      | v == var  = (var, val) : vs
+      | otherwise = (v, sVal) : updateStore var val vs
+run ((Fetch varName):code, stack, state@(_, store)) =
+    case lookup varName store of
+        Just val -> trace ("Fetch " ++ varName) $ run (code, val : stack, state)
+        Nothing  -> error "Run-time error"  -- Adjusted error message to match the requirement
+run (Neg:code, BVal b : stack, state) =
+    trace ("Neg") $
+    run (code, BVal (not b) : stack, state)
+run (Neg:code, stack, state) =
+    error "Neg instruction expects a boolean value on top of the stack"
+run (Equ:code, v1 : v2 : stack, state) =
+    trace ("Equ") $
+    run (code, BVal (v1 == v2) : stack, state)
+run (Le:code, IVal n1 : IVal n2 : stack, state) =
+    trace ("Le") $
+    run (code, BVal (n1 <= n2) : stack, state)  -- Ensure that n1 is the last pushed value
+run (Le:_, _, _) =
+    error "Le instruction requires two integer values on top of the stack"
+run (Loop condition body:restCode, stack, state) = 
+    let (_, conditionStack, _) = run (condition, stack, state)
+    in if getConditionResult conditionStack
+       then let (_, bodyStack, bodyState) = run (body, stack, state)
+            in run (Loop condition body:restCode, bodyStack, bodyState)
+       else run (restCode, stack, state)
+run (And:code, BVal b1 : BVal b2 : stack, state) =
+    trace ("And") $
+    run (code, BVal (b1 && b2) : stack, state)
+run (And:_, _, _) =
+    error "Run-time error: 'And' operation requires two boolean values on top of the stack"
+
 -- Implement other instructions as needed.
 
 
@@ -79,17 +126,17 @@ testAssembler code = (stack2Str stack, state2Str state)
 
 main :: IO ()
 main = do
-  let testResult = testAssembler [Fals, Push 3, Tru, Store "var", Store "a", Store "someVar"]
+  let testResult = testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]]
   let (stackStr, stateStr) = testResult -- Unpack the tuple into stackStr and stateStr
   putStrLn "Stack:"
   putStrLn stackStr -- Corrected to use stackStr
   putStrLn "State:"
   putStrLn stateStr -- Corrected to use stateStr
-  let expectedResult = ("", "a=3,someVar=False,var=True")
+  let expectedResult = ("","fact=3628800,i=1")
   print (testResult == expectedResult) -- This compares the entire tuple
   -- The following lines are likely not necessary if the above print statement is what you want
-  -- print (stackStr == fst expectedResult) -- Compare stack string to expected stack string
-  -- print (stateStr == snd expectedResult) -- Compare state string to expected state string
+  print (stackStr == fst expectedResult) -- Compare stack string to expected stack string
+  print (stateStr == snd expectedResult) -- Compare state string to expected state string
   -- If you still want to print stackStr and stateStr separately, you can uncomment these lines:
-  -- print stackStr  -- Print the actual stack string
-  -- print stateStr  -- Print the actual state string
+  print stackStr  -- Print the actual stack string
+  print stateStr  -- Print the actual state string
